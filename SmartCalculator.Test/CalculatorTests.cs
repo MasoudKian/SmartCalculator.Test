d5199caa894620a605416af2ae7ec775da62a587
Masoud Kian <masoudkiannezhad@gmail.com>
2025-07-13 23:18:19 +0330

    Add project files.

---
+++ SmartCalculator.Test/CalculatorTests.cs
@@ -0,0 +1,51 @@
+﻿using SmartCalculator.Test;
+
+namespace SmartCalculator.Tests;
+
+public class CalculatorTests
+{
+    private readonly Calculator _calc = new();
+
+    [Fact]
+    public void Add_TwoNumbers_ReturnsSum()
+    {
+        var result = _calc.Add(2, 3);
+        Assert.Equal(5, result);
+    }
+
+    [Fact]
+    public void Subtract_TwoNumbers_ReturnsDifference()
+    {
+        var result = _calc.Subtract(5, 3);
+        Assert.Equal(2, result);
+    }
+
+    [Fact]
+    public void Multiply_TwoNumbers_ReturnsProduct()
+    {
+        var result = _calc.Multiply(4, 3);
+        Assert.Equal(12, result);
+    }
+
+    [Fact]
+    public void Divide_TwoNumbers_ReturnsQuotient()
+    {
+        var result = _calc.Divide(10, 2);
+        Assert.Equal(5, result);
+    }
+
+    [Fact]
+    public void Divide_ByZero_ThrowsException()
+    {
+        Assert.Throws<DivideByZeroException>(() => _calc.Divide(10, 0));
+    }
+
+    [Theory]
+    [InlineData(2, 3, 8)]
+    [InlineData(4, 0.5, 2)]
+    public void Power_BaseAndExponent_ReturnsResult(double b, double e, double expected)
+    {
+        var result = _calc.Power(b, e);
+        Assert.Equal(expected, result, 5);
+    }
+}
---
+++ SmartCalculator/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCalculator.Test
+{
+
+    public class Calculator
+    {
+        public double Add(double a, double b) => a + b;
+
+        public double Subtract(double a, double b) => a - b;
+
+        public double Multiply(double a, double b) => a * b;
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Cannot divide by zero.");
+            return a / b;
+        }
+
+        public double Power(double baseNum, double exponent) =>
+            Math.Pow(baseNum, exponent);
+    }
+}
