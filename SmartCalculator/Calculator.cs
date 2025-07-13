5e8f4ce7d8b887c21575c75ef3c1021809bb5d25
Masoud Kian <masoudkiannezhad@gmail.com>
2025-07-13 23:43:11 +0330

    test

--- SmartCalculator/Calculator.cs
+++ SmartCalculator/Calculator.cs
@@ -16,6 +16,7 @@ namespace SmartCalculator.Test
         public double Multiply(double a, double b) => a * b;
 
         public double Divide(double a, double b)
+
         {
             if (b == 0)
                 throw new DivideByZeroException("Cannot divide by zero.");
